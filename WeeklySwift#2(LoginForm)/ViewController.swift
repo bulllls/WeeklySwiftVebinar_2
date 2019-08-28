306d912abe23f573cafea4d24b2f833bb07556f7
Ruslan <ruslanfilistovich@gmail.com>
2019-08-28 19:40:00 +0300

    Initial Commit

---
+++ WeeklySwift#2(LoginForm)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeeklySwift#2(LoginForm)
+//
+//  Created by Ruslan on 8/28/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
