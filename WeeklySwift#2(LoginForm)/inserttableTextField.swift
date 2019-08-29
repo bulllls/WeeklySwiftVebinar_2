795f1967e1123021e91c6f5e932aa20ccebd97a8
Ruslan <ruslanfilistovich@gmail.com>
2019-08-29 18:18:06 +0300

    style input form

--- WeeklySwift#2(LoginForm)/inserttableTextField.swift
+++ WeeklySwift#2(LoginForm)/inserttableTextField.swift
@@ -13,6 +13,11 @@ class inserttableTextField: UITextField {
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.borderWidth = 1
-        self.layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        self.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        self.layer.cornerRadius = 5
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 8, dy: 7)
     }
 }
